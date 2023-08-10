fc976ef7fefc2b3a7adc4af46fb3faa52660ba5e
engrosamaali91 <engr.osamaali91@gmail.com>
2023-08-10 12:45:39 +0200

    Json file updated and test_shape_DNA using fixture method

--- lapy/test_shape_DNA.py
+++ lapy/test_shape_DNA.py
@@ -1,8 +1,9 @@
-# imports
 from lapy import TriaMesh, TetMesh
 from lapy import shapedna
 import os
 import numpy as np
+import json
+import pytest
 
 # Loading data
 data_dir = os.path.join(os.path.dirname(__file__), "..", "data")
@@ -11,63 +12,88 @@ vtk_filename_2 = "cubeTetra.vtk"
 tria = TriaMesh.read_vtk(os.path.join(data_dir, vtk_filename_1))
 tet = TetMesh.read_vtk(os.path.join(data_dir, vtk_filename_2))
 
-
-
 # compute eigenvalues and eigenvectors for tria mesh
 ev = shapedna.compute_shapedna(tria, k=3)
 ev['Eigenvectors']
 ev['Eigenvalues']
 
-def test_compute_shapedna():
 
+@pytest.fixture
+def loaded_data():
     """
-    This function performs unit testing for the compute_shapedna() function for tria mesh by
-    comparing the computed Eigenvalues with expected values using a specified tolerance.
-    Additionally, it asserts that the dtype of the computed Eigenvalues is float32.
-    """
-    expected_Eigenvalues = np.array([-4.0165149e-05,  4.1696410e+00,  4.1704664e+00])
-    tolerance = 1e-4  # Adjust the tolerance value as needed
+    Load and provide the expected outcomes data from a JSON file.
     
-    # assert np.isclose(ev['Eigenvalues'], expected_Eigenvalues, tolerance).all()
+    Returns:
+        dict: Dictionary containing the expected outcomes data.
+    """
+    with open('expected_outcomes.json', 'r') as f:
+        expected_outcomes = json.load(f)
+    return expected_outcomes
+
+def test_compute_shapedna(loaded_data):
+    """
+    Test the compute_shapedna function for a triangular mesh.
+    
+    Parameters:
+        loaded_data (dict): Expected outcomes data loaded from a JSON file.
+        
+    Raises:
+        AssertionError: If the computed eigenvalues do not match the expected values within tolerance.
+        AssertionError: If the dtype of the computed eigenvalues is not float32.
+    """
+    expected_Eigenvalues = np.array(loaded_data["expected_outcomes"]["test_compute_shapedna"]["expected_eigenvalues"])
+    tolerance = loaded_data["expected_outcomes"]["test_compute_shapedna"]["tolerance"]
     assert np.allclose(ev['Eigenvalues'], expected_Eigenvalues, atol=tolerance)
     assert ev['Eigenvalues'].dtype == np.float32
 
-def test_normalize_ev_geometry():
-
+def test_normalize_ev_geometry(loaded_data):
     """
-    This function performs unit testing for the normalize_ev() function using the
-    'geometry' method. It compares the normalized eigenvalues with expected values.
-    It also compares whether the data type of the normalized is float32
-    """
-    expected_normalized_values = np.array([-2.4099089e-04, 2.5017845e+01, 2.5022799e+01])
-    tolerance = 1e-4
+    Test the normalize_ev() function using the 'geometry' method for a triangular mesh.
     
+    Parameters:
+        loaded_data (dict): Expected outcomes data loaded from a JSON file.
+        
+    Raises:
+        AssertionError: If the normalized eigenvalues do not match the expected values within tolerance.
+        AssertionError: If the dtype of the normalized eigenvalues is not float32.
+    """
+    expected_normalized_values = np.array(loaded_data["expected_outcomes"]["test_normalize_ev_geometry"]["expected_normalized_values"])
+    tolerance = loaded_data["expected_outcomes"]["test_normalize_ev_geometry"]["tolerance"]
     normalized_eigenvalues = shapedna.normalize_ev(tria, ev['Eigenvalues'], method="geometry")
-    
     assert np.allclose(normalized_eigenvalues, expected_normalized_values, atol=tolerance)
     assert normalized_eigenvalues.dtype == np.float32
 
 
-def test_reweight_ev():
-
+def test_reweight_ev(loaded_data):
     """
-    This function performs unit testing for reweighted_ev() function.
-    It also compares whether the data type of the reweighted eigenvalues is float32
+    Test the reweighted_ev() function and validate the data type of the reweighted eigenvalues.
+    
+    Parameters:
+        loaded_data (dict): Expected outcomes data loaded from a JSON file.
+        
+    Raises:
+        AssertionError: If the reweighted eigenvalues do not match the expected values within tolerance.
+        AssertionError: If the dtype of the reweighted eigenvalues is not float32.
     """
-    expected_reweighted_values = np.array([-4.01651487e-05,  2.08482051e+00,  1.39015547e+00])
-    original_eigenvalues = np.copy(ev['Eigenvalues'])
+    expected_reweighted_values = np.array(loaded_data["expected_outcomes"]["test_reweight_ev"]["expected_reweighted_values"])
+    tolerance = loaded_data["expected_outcomes"]["test_reweight_ev"]["tolerance"]
     reweighted_eigenvalues =  shapedna.reweight_ev(ev['Eigenvalues'])
     tolerance = 1e-4
-    assert np.allclose(reweighted_eigenvalues, expected_reweighted_values, atol=tolerance)
-    
+    assert np.allclose(reweighted_eigenvalues, expected_reweighted_values, atol=tolerance)    
 
-def test_compute_distance():
+def test_compute_distance(loaded_data):
     """
-    This function performs unit testing for compute_distance() function.
+    Test the compute_distance() function for eigenvalues and validate the computed distance.
+    
+    Parameters:
+        loaded_data (dict): Expected outcomes data loaded from a JSON file.
+        
+    Raises:
+        AssertionError: If the computed distance does not match the expected value.
     """
+    expected_compute_distance = loaded_data["expected_outcomes"]["test_compute_distance"]["expected_compute_distance"]
     # compute distance for tria eigenvalues (trivial case)
     computed_distance = shapedna.compute_distance(ev["Eigenvalues"], ev["Eigenvalues"])
-    expected_compute_distance = 0.0
     assert computed_distance == expected_compute_distance
 
 # Repeat testing steps for a tetrahedral mesh
@@ -76,26 +102,36 @@ evTet = shapedna.compute_shapedna(tet, k=3)
 evTet['Eigenvectors']
 evTet['Eigenvalues']
 
-def test_compute_shape_dna():
+def test_compute_shapedna_tet(loaded_data):
     """
-    This functoin performs unit testing for compute_shapedna function for tet mesh
+    Test the compute_shapedna function for a tetrahedral mesh.
+    
+    Parameters:
+        loaded_data (dict): Expected outcomes data loaded from a JSON file.
+        
+    Raises:
+        AssertionError: If the computed eigenvalues do not match the expected values within tolerance.
+        AssertionError: If the dtype of the computed eigenvalues is not float32.
     """
-    tolerance = 1e-4
-    expected_eigen_values = np.array([8.4440224e-05, 9.8897915e+00, 9.8898811e+00])
-    # evTet = shapedna.compute_shapedna(tet, k=3)
+    expected_eigen_values = np.array(loaded_data["expected_outcomes"]["test_compute_shapedna_tet"]["expected_eigen_values"])
+    tolerance = loaded_data["expected_outcomes"]["test_compute_shapedna_tet"]["tolerance"]
+    evTet = shapedna.compute_shapedna(tet, k=3)
     assert np.allclose(evTet['Eigenvalues'], expected_eigen_values, atol=tolerance)
     assert evTet['Eigenvalues'].dtype == np.float32
     
-def test_normalize_ev_geometry_tet():
-
+def test_normalize_ev_geometry_tet(loaded_data):
     """
-    This function performs unit testing for the normalize_ev() function for tet mesh using the
-    'geometry' method. It compares the normalized eigenvalues with expected values.
-    It also compares whether the data type of the normalized is float32
+    Test the normalize_ev() function using the 'geometry' method for a tetrahedral mesh.
+    
+    Parameters:
+        loaded_data (dict): Expected outcomes data loaded from a JSON file.
+        
+    Raises:
+        AssertionError: If the normalized eigenvalues do not match the expected values within tolerance.
+        AssertionError: If the dtype of the normalized eigenvalues is not float32.
     """
-    expected_normalized_values = np.array([8.4440224e-05, 9.8897915e+00, 9.8898811e+00])
-    tolerance = 1e-4
-    # evTet = shapedna.compute_shapedna(tet, k=3)
+    expected_normalized_values = np.array(loaded_data["expected_outcomes"]["test_normalize_ev_geometry_tet"]["expected_normalized_values"])
+    tolerance = loaded_data["expected_outcomes"]["test_normalize_ev_geometry_tet"]["tolerance"]
     # volume / surface / geometry normalization of tet eigenvalues
     normalized_eigenvalues = shapedna.normalize_ev(tet, evTet['Eigenvalues'], method="geometry")
     
@@ -103,26 +139,39 @@ def test_normalize_ev_geometry_tet():
     assert normalized_eigenvalues.dtype == np.float32
 
 
-def test_reweight_ev_tet():
-
+def test_reweight_ev_tet(loaded_data):
     """
-    This function performs unit testing for reweighted_ev() function for tet meshes.
-    It also compares whether the data type of the reweighted eigenvalues is float32
+    Test the reweighted_ev() function for tetrahedral meshes and validate the data type of the reweighted eigenvalues.
+    
+    Parameters:
+        loaded_data (dict): Expected outcomes data loaded from a JSON file.
+        
+    Raises:
+        AssertionError: If the reweighted eigenvalues do not match the expected values within tolerance.
+        AssertionError: If the dtype of the reweighted eigenvalues is not float32.
     """
-    expected_reweighted_values = np.array([8.44402239e-05, 4.94489574e+00, 3.29662704e+00])
+    expected_reweighted_values = np.array(loaded_data["expected_outcomes"]["test_reweight_ev_tet"]["expected_reweighted_values"])
+    tolerance = loaded_data["expected_outcomes"]["test_reweight_ev_tet"]["tolerance"]
     # Linear reweighting of tet eigenvalues
     reweighted_eigenvalues =  shapedna.reweight_ev(evTet['Eigenvalues'])
-    tolerance = 1e-4
     assert np.allclose(reweighted_eigenvalues, expected_reweighted_values, atol=tolerance)
     
 
-def test_compute_distance_tet():
+def test_compute_distance_tet(loaded_data):
     """
-    This function performs unit testing for compute_distance() function.
+    Test the compute_distance() function for eigenvalues of tetrahedral meshes and validate the computed distance.
+    
+    Parameters:
+        loaded_data (dict): Expected outcomes data loaded from a JSON file.
+        
+    Raises:
+        AssertionError: If the computed distance does not match the expected value.
     """
     # compute distance for tria eigenvalues (trivial case)
     computed_distance = shapedna.compute_distance(evTet["Eigenvalues"], evTet["Eigenvalues"])
-    expected_compute_distance = 0.0
+    expected_compute_distance = loaded_data["expected_outcomes"]["test_compute_distance_tet"]["exp_compute_distance"]
     
     # Compare the computed distance with the expected distance using a tolerance
     assert computed_distance == expected_compute_distance
+
+
